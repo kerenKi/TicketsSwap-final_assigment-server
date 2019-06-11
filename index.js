564ad2b9a8f31f1a2a704edb6c501dc9ddf94862
kerenKi <kkinberg13@gmail.com>
2019-06-11 13:07:17 +0200

    comment wrong routes

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express =  require('express');
 const bodyParser = require('body-parser')
 const cors = require('cors')
+//later import the relevant routers
 // const productsRouter = require('./src/products/routes')
+
 
 
 const app = express()
@@ -10,5 +12,6 @@ const port = process.env.PORT || 4000
 app
   .use(cors())
   .use(bodyParser.json())
+  //later use relevant routers
   // .use(productsRouter)
   .listen(port, () => console.log(`Listening on port ${port}`))
